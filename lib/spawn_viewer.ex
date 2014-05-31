46219080a8d0ade5d4a3d511049f32c814f8b459
parroty <parroty00@gmail.com>
2014-05-31 10:30:18 +0900

    Updates for elixir v0.13.3

--- lib/spawn_viewer.ex
+++ lib/spawn_viewer.ex
@@ -31,7 +31,7 @@ defmodule SpawnViewer do
   end
 
   defp format_item(dict) do
-    Enum.map(dict.keys, fn(pid) ->
+    Enum.map(HashDict.keys(dict), fn(pid) ->
       item = HashDict.get(dict, pid)
       PlotItem.new(name: inspect(pid), tag: item[:tag],
                    start_time: format_time(item[:start]), end_time: format_time(item[:end]),
@@ -44,7 +44,7 @@ defmodule SpawnViewer do
   end
 
   defp format_time({_, _, microsec} = time) do
-    Timex.Date.from(:calendar.now_to_universal_time(time)).update(ms: Float.floor(microsec / 1000))
+    %{Timex.Date.from(:calendar.now_to_universal_time(time)) | ms: Float.floor(microsec / 1000)}
   end
 
   defp format_events(nil) do
