b6e4ede760c03f0cad842ccf11821e36db039a93
parroty <parroty00@gmail.com>
2015-12-31 21:22:19 +0900

    Fix test

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule SpawnViewer.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Spawn Viewer"
   end
 end
